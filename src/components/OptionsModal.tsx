fadf3d3a0d12df8b1923f3adf8c52f402c33bdd2
Marc Made It <spurdo.dk@gmail.com>
2024-09-03 10:41:19 +0200

    Update OptionsModal.tsx

--- src/components/OptionsModal.tsx
+++ src/components/OptionsModal.tsx
@@ -17,7 +17,7 @@ interface Props {
       className={`absolute -top-[170px] right-0 z-60 ${show ? "show" : ""} md:top-12 md:-right-7 md:p-3 rounded-b-xl`}
       onClick={(e) => e.stopPropagation()}
     >
-      <div className=" flex flex-col gap-3 text-center cursor-default">
+      <div className=" flex flex-col gap-3 text-start cursor-default">
         <Link
           className=" border-2 border-primary rounded-xl px-5 py-2 bg-background hover:border-secondary hover:text-secondary transition-colors duration-300"
           href="/petcare"
