bcb0a91fa210870126439b2a593e15b5f8f17249
Marc Made It <spurdo.dk@gmail.com>
2024-09-04 17:54:56 +0200

    Update MenuRight.tsx

--- src/components/menuRight/MenuRight.tsx
+++ src/components/menuRight/MenuRight.tsx
@@ -6,7 +6,7 @@ import UserInfoCard from './UserInfoCard'
 import UserMediaCard from './UserMediaCard'
 import { User } from '@prisma/client'
 
-const MenuRight = ({user}:{user: User}) => {
+const MenuRight = ({user}:{user?: User}) => {
   return (
     <div className='flex flex-col gap-5'>
       {user ? (
