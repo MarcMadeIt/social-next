65856740077f550868ec4baf300825f8b2a754f7
Marc Made It <spurdo.dk@gmail.com>
2024-10-28 15:26:38 +0100

    Update client.ts

--- src/lib/client.ts
+++ src/lib/client.ts
@@ -1,5 +1,5 @@
 // client.ts
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "../../prisma/generated/client";
 
 const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
 
